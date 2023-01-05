7b53998a7b2661da94494aa82bc64ecc7489fafb
weatherdude <97835234+weatherdude@users.noreply.github.com>
2023-01-05 08:48:40 +0100

    some minor updates

--- overlay_pred_to_video.py
+++ overlay_pred_to_video.py
@@ -1,38 +1,41 @@
 import pickle
 import cv2
-import numpy as np
 import pandas as pd
 from pytictoc import TicToc
 from matplotlib.font_manager import json_load
-from scipy.spatial import distance
 from utility import *
-import math
-t = TicToc() #create instance of class
+
+t = TicToc() #create instance of timer
 
 # type of font to be used later on
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 # import trained classification model
-with open('E:\Dokumente\Repos\kinematics-main\kinematics-main\model_HIVECOTEV2_bird_plant_plane_30.pkl', 'rb') as f:
+with open('E:\Dokumente\Repos\kinematics-main\kinematics-main\model_Catch22_bird_plant_plane_30.pkl', 'rb') as f:
     clf = pickle.load(f)
 
+# path
+# Bird
+path = 'E:\\Dokumente\\UFO\\vectoring_guiding\\Processed\\7-done\\7-done\\tracked\\78629d4c-4534-44d6-a22e-e995aa8b9f43_002147'
+
 # import video
-#Plane
-cap = cv2.VideoCapture('E:\\Dokumente\\UFO\\vectoring_guiding\\Processed\\7-done\\7-done\\tracked\\7a313d0f-c06a-465b-9d5a-68e16fd3d378_002507\\annotated_video.mp4')
-#cap = cv2.VideoCapture('E:\\Dokumente\\UFO\\vectoring_guiding\\Processed\\7-done\\7-done\\tracked\\87\\annotated_video.mp4')
-#cap = cv2.VideoCapture('E:\\Sky360_videos\\5\\54a2d385-88cd-49c2-99ef-76ebb4ae616a_000871\\annotated_video.mp4')
+cap = cv2.VideoCapture(path+'\\annotated_video.mp4')
 
 # get annotations for video
-#Plane
-df = json_load('E:\\Dokumente\\UFO\\vectoring_guiding\\Processed\\7-done\\7-done\\tracked\\7a313d0f-c06a-465b-9d5a-68e16fd3d378_002507\\annotations.json')
-#df = json_load('E:\\Dokumente\\UFO\\vectoring_guiding\\Processed\\7-done\\7-done\\tracked\\87\\annotations.json')
-#df = json_load('E:\\Sky360_videos\\5\\54a2d385-88cd-49c2-99ef-76ebb4ae616a_000871\\annotations.json')
+df = json_load(path+'\\annotations.json')
+
 # track of interest
-track = 5
+track = 34
+
+# number of frames for doing predictions
+sequence_length = 30
 
 # Check if camera opened successfully
 if (cap.isOpened()== False):
     print("Error opening video stream or file")
+
+# get video resolution
+height,width = video_size(path+"\\annotated_video.mp4")
 
 bbox_center = []
 vector_list = []
@@ -60,8 +63,8 @@ while(cap.isOpened()):
         if len(bbox_center)>3:
             # calculate distance and direction vector
 
-            _,dist = distance_vector(bbox_center[-1],bbox_center[-2])
-            vector,_ = distance_vector(bbox_center[-1],bbox_center[-4])
+            _,dist = distance_vector(bbox_center[-1],bbox_center[-2],height,width)
+            vector,_ = distance_vector(bbox_center[-1],bbox_center[-4],height,width)
             vector_list.append(vector)
             dist_list.append(dist)
 
@@ -69,7 +72,7 @@ while(cap.isOpened()):
                 theta = direction_change(vector_list[-1],vector_list[-2])
                 direction_change_list.append(theta)
 
-        if len(dist_list) ==30: # apply model every 30 frames
+        if len(dist_list) ==sequence_length: # apply model every x frames
 
             # direction_change_list contains NaNs here and there
             # this happens when the magnitude of a direction vector is 0
@@ -84,17 +87,17 @@ while(cap.isOpened()):
             # prepare dataframe with input data for classification model
             d = {}
             m = 0
-            for j in range(0,30):
-                if j % 30 == 0:
-                  d[m] = pd.Series(direction_change_list_cleaned[j:j+30])
+            for j in range(0,sequence_length):
+                if j % sequence_length == 0:
+                  d[m] = pd.Series(direction_change_list_cleaned[j:j+sequence_length])
                   m += 1
             df1 = pd.Series(d).to_frame('Direction_changes')
 
             d = {}
             m = 0
-            for j in range(0,30):
-                if j % 30 == 0:
-                  d[m] = pd.Series(dist_list[j:j+30])
+            for j in range(0,sequence_length):
+                if j % sequence_length == 0:
+                  d[m] = pd.Series(dist_list[j:j+sequence_length])
                   m += 1
             df2 = pd.Series(d).to_frame('Distances')
 
@@ -142,7 +145,7 @@ cv2.destroyAllWindows()
 
 # write video
 height,width,layers=img[0].shape
-video=cv2.VideoWriter('video_plane_5_HIVECOTEV2.mp4',-1,fps=15,frameSize=(width,height))
+video=cv2.VideoWriter('video_bird_5_Catch22_norm.mp4',-1,fps=15,frameSize=(width,height))
 for j in range(0,len(img)):
     video.write(img[j])
 video.release()
